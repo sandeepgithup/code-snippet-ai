14392268c4369f2a03ff6f29a1aaf1bb4ccfdc65
sandeepgithup <sandeepambala31@gmail.com>
2025-07-15 11:58:23 +0000

    Initial commit - DocTypes and app setup

---
+++ code_snippet_ai/code_snippet_ai/doctype/code_snippet/code_snippet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sandeep and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Code Snippet", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ code_snippet_ai/code_snippet_ai/doctype/code_snippet_category/code_snippet_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, sandeep and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CodeSnippetCategory(Document):
+	pass
